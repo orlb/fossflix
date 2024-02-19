9891fb2b8d154e91ecdf22aaf5d38a05c32c566c
Orlandis <olbrown3@alaska.edu>
2024-02-19 01:38:14 -0900

    Prototype: authentication flow

--- src/local/app.js
+++ src/local/app.js
@@ -2,21 +2,76 @@
 //
 //  Package entry point, express.js startup, configuration and routes
 
-const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser');
 
 const user = require('./modules/auth');
 
+
 const app = express();
+app.use(cookieParser()) // get cookies object at req.cookies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function enforceSession (req, res) {
+    // return: true | false
+    // call at the start of every route where user must be authenticated
+    // send user to login if not authenticated
+
+    if ( user.updateUserSession(req, res) == true ) {
+        return true;
+    }
+    else {
+        // save referrer, redirect back on successful login
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
+        let encoded_redirect_url = encodeURIComponent(req.url);
+        res.redirect(`/login?ref=${encoded_redirect_url}`);
+        return false;
+    }
+}
 
 app.get('/', function(req, res) {
+    if ( ! enforceSession(req, res) ) {
+        return;
+    }
+    res.send(`If you've made it this far, you are probably authenticated.`)
+});
+
+app.get('/login', function(req, res) {
     res.sendFile(path.join(__dirname, 'html/index.html'));
 });
 
-app.post('/access', function(req, res) {
-    console.log("User attempted to login / register");
-    res.redirect('/?a=login_success');
+app.post('/login', function(req, res) {
+    if ( req.body.login_form.register ) {
+        // login form 'register' button clicked
+        if ( user.registerUserCredentials(req).valid == true )
+        {
+            // send back success code to browser
+            res.sendStatus(200);
+        }
+        else {
+            res.sendStatus(400);
+        }
+    }
+    else if ( req.body.login_form.login ) {
+        let login_status = user.authenticateUser(req) 
+        if ( login_status.valid ) {
+            if ( req.query.ref ) {
+                let decoded_redirect_url = decodeURIComponent(req.query.ref);
+                res.redirect(`${decoded_redirect_url}`);
+            }
+            else {
+                res.redirect(`/`);
+            }
+        }
+        else {
+            res.sendStatus(400).json({ attempts: login_status.attempts });
+        }
+    }
+    res.sendStatus(400);
 });
 
 app.listen(8443, '0.0.0.0');
-console.log('Server started at http://localhost');
+console.log('Server started at http://localhost:8443');
--- src/local/modules/auth.js
+++ src/local/modules/auth.js
@@ -1,47 +1,88 @@
 //  auth.js
 //
 //  Provides user handling functions
+//  Requires: fs, Express, cookie-parser
 
-function enforceCredentialRequirements () {
-    // return: { True | False,  error code }
+function enforceCredentialRequirements (req) {
+    // return: { true | false,  error code }
     // succeeds client-side credential requirement checks
+
+    let credential_status = { valid: true };
+    return credential_status;
+}
+
+function createUserRecord () {
+    // return: true | false
+    // write to user data file
+
+    return true;
+}
+
+function deleteUserRecord () {
+    // return: true | false
+    // remove user data
 }
 
 function validateUserCredentials () {
-    // return: { True | False, error code }
-    // Credential checking procedure
+    // return: { true | false, attempts }
+    // credential checking procedure
+    // use req.body.username, etc
+
+    return { valid: true };
 }
 
 function createUserSession () {
-    // return: User session ID | False
+    // return: user session ID | false
     // create user session on login
 }
 
 function deleteUserSession () {
-    // return: User session ID | False
+    // return: user session ID | false
     // delete user session on logout
 }
 
 function getUserSession () {
-    // return: User session ID | False
+    // return: user session ID | false
     // query for user session
 }
 
 module.exports = {
 
-    registerUserCredentials: function () {
-        // return: { True | False, error code }
+    registerUserCredentials: function (req) {
+        // return: { true | false, error code }
         // data entry with user credentials
-        console.log('hi');
+
+        let registration_status = enforceCredentialRequirements(req);
+
+        if ( registration_status.valid == true ) {
+            if ( createUserRecord(req) == true ) {
+                return registration_status;
+            }
+        }
+        else {
+            return registration_status;
+        }
     },
 
-    authenticateUser: function () {
-        // return: User session ID | False
+    authenticateUser: function (req) {
+        // return: user session ID | false
+        // set session ID, return response to client browser, client initiated redirect if successful
+        let login_status = validateUserCredentials(req);
+        
+        if ( login_status.valid == true ) {
+            login_status.sessionid = createUserSession(req);
+            return login_status;
+        }
+        else {
+            return login_status;
+        }
     },
 
-    updateUserSession: function () {
-        // return: True | False
+    updateUserSession: function (req, res) {
+        // return: true | false
         // check session expiration, if it exists, and other things
+        
+        return false;
     },
 
 }
