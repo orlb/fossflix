455428e1cfc4d972bf4191664a1232684d50c3e6
Andrew Mattson <acmattson3@alaska.edu>
2024-03-20 12:44:35 -0800

    Added queries for landing page and movie searching/removing/updating.

--- src/api.js
+++ src/api.js
@@ -5,16 +5,42 @@ const express = require('express');
 const path = require('path');
 
 const user = require('./modules/user');
-// const query = require('./modules/query');
+const query = require('./modules/query');
 
 const router = express.Router();
 
-router.get('/api/movies/SearchMovies', user.enforceSession, function(req, res) {
-    // query api
+// Search movies
+router.get('/api/movies/search', function(req, res) {
+    const title = req.query.title;
+    const sortMethod = 'uploadDate'; // Assuming you want to sort by upload date
+    query.searchMovie(title, sortMethod)
+        .then(movies => res.json(movies))
+        .catch(err => res.status(500).send(err.message));
 });
 
-router.get('/api/movies/UpdateMovie', user.enforceSession, function(req, res) {
-    // query api
+// Add movie
+router.post('/api/movies/add', function(req, res) {
+    const { filename, metadata } = req.body;
+    query.addMovie(filename, metadata)
+        .then(result => res.status(201).json(result))
+        .catch(err => res.status(500).send(err.message));
+});
+
+// Remove movie
+router.delete('/api/movies/:id', function(req, res) {
+    const movieId = req.params.id;
+    query.removeMovie(movieId)
+        .then(code => res.sendStatus(code))
+        .catch(err => res.status(500).send(err.message));
+});
+
+// Update movie
+router.put('/api/movies/update/:id', function(req, res) {
+    const movieId = req.params.id;
+    const metadata = req.body.metadata;
+    query.updateMovie(movieId, metadata)
+        .then(result => res.json(result))
+        .catch(err => res.status(500).send(err.message));
 });
 
 module.exports = router;
--- src/modules/query.js
+++ src/modules/query.js
@@ -1,33 +1,69 @@
 const config = require("config");
 const { MongoClient } = require("mongodb");
 const uri = config.get("mongodb_uri");
-const database = config.get("mongodb_db");
+const database_name = config.get("mongodb_db");
 
 const client = new MongoClient(uri);
-const database = client.db(database);
-const movies = database.collection("users");
+const database = client.db(database_name);
+const movies = database.collection("movies");
+const users = database.collection("users");
 
-async function _query_database (query) {
-  try {
-    let fieldName = "fname";
 
-    let result_set = await movies.distinct(fieldName, query);
-    return result_set;
-  } finally {
-	  console.log("query failed");
-  }
-}
 
-return {
-	getUsers = function (index) {
+const searchMovie = async (title, sortMethod) => {
+    try {
+        await client.connect();
+        const query = title ? { title: { $regex: title, $options: "i" } } : {};
+        return movies.find(query).sort({ [sortMethod]: -1 }).toArray();
+    } finally {
+        await client.close();
+    }
+};
+
+const addMovie = async (filename, metadata) => {
+    try {
+        await client.connect();
+        const result = await movies.insertOne({ filename, metadata });
+        return { code: 201, record: result.ops[0] };
+    } finally {
+        await client.close();
+    }
+};
+
+const removeMovie = async (movieId) => {
+    try {
+        await client.connect();
+        await movies.deleteOne({ _id: new MongoClient.ObjectID(movieId) });
+        return 204; // 'No Content'
+    } finally {
+        await client.close();
+    }
+};
+
+const updateMovie = async (movieId, metadata) => {
+    try {
+        await client.connect();
+        const result = await movies.updateOne({ _id: new MongoClient.ObjectID(movieId) }, { $set: metadata });
+        return { code: 200, record: result.ops[0] }; // In practice, we should return the updated document
+    } finally {
+        await client.close();
+    }
+};
+
+module.exports = {
+	getUsers: function (index) {
 	    let query = { fname: index };
-		let users = {
-		}
+		let users = {}
 		let result_set = connection.find(query);
 		return result_set;
 	},
 
-	close = function () {
+	close: async function () {
 	    await client.close();
-	}
+	},
+
+    searchMovie,
+    addMovie,
+    removeMovie,
+    updateMovie
 };
